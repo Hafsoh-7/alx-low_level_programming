dbc6aad28a2afaa299c68f57387a5f33ff3c7e9a
Bolajoko Hafsoh <hafsohbolajoko7@gmail.com>
2023-04-21 05:44:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - code that print a string
+/* 
+ * main - This is a code for string
  * Return: 0
  */
 int main(void)
 {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0)
+	printf("with proper grammar, but the outcome is a piece of art,/n")
+	return(0)
 }
