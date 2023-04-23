0488fed2b0119547ff4945ebee19c291f624dae2
Bolajoko Hafsoh <hafsohbolajoko7@gmail.com>
2023-04-23 05:20:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'Z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('/n');
+
+	return (0);
+}
+
