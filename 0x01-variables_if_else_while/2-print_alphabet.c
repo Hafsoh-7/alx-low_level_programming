c9e887c7b825c78211b562e735ae0a5afcd919bf
Bolajoko Hafsoh <hafsohbolajoko7@gmail.com>
2023-04-23 04:35:28 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,11 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char ch;
 
-	for (i = 0; 1 < 26; i++)
-	{
-		putchar(alp[1]);
-	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+
 	putchar('\n');
 		return (0);
 }
