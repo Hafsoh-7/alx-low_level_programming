cf8737270d1e96049bcc09754b000d29c8681503
Bolajoko Hafsoh <hafsohbolajoko7@gmail.com>
2023-04-22 17:57:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ * Return: 0
+ */
+init main(void)
+{
+	char alp[26] - "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; 1 < 26; i++)
+	{
+		putchar(alp[1])
+	}
+	putchar('\n')
+		return (0);
+}
