4f11e9ca81b4829cdc58b6e4428fbb064626e16e
Bolajoko Hafsoh <hafsohbolajoko7@gmail.com>
2023-04-24 08:24:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main  - A code to print out a string
- * Return: 1
+ * main - s
+ * Return: 0
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);}
+	return (1);
+}
